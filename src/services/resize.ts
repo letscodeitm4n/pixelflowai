11f0c8ccb019a00f4bc8e1ba6e3899da7529effb
letscodeitm4n <rgarcia11011990@gmail.com>
2026-07-27 23:33:40 +0800

    Update src/services/resize.ts

--- src/services/resize.ts
+++ src/services/resize.ts
@@ -1,7 +1,7 @@
 import sharp from 'sharp';
 import { Request, Response } from 'express';
-import { fetchImageBuffer, ApiError } from '../utils/fetch-image';
-import { sendSuccess, sendError, bufferToBase64 } from '../utils/response';
+import { fetchImageBuffer, ApiError } from '../utils/fetch-image.js';
+import { sendSuccess, sendError, bufferToBase64 } from '../utils/response.js';
 
 export async function resizeHandler(req: Request, res: Response): Promise<void> {
   try {
